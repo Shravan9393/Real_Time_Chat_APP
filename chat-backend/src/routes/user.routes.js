5ae2144e1a6d817d3bf21ef84244f570f06d28fd
Shravan Yadav <yadavshravan9393@gmail.com>
2025-01-13 21:41:29 +0530

    feat: add user profile update routes and enhance user retrieval functionality

--- chat-backend/src/routes/user.routes.js
+++ chat-backend/src/routes/user.routes.js
@@ -4,6 +4,11 @@ import {
   loginUser,
   logoutUser,
   refreshAccessToken,
+  searchUser,
+  getUserById,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -37,10 +42,10 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/searchUser").get(verifyJWT, searchUser);
-router.route("/getUserById").get(verifyJWT, getUserById);
-router.route("updateAccountDetails").patch(verifyJWT, updateAccountDetails);
-router.route("updateUserAvatar").patch(verifyJWT, updateAvatar);
-router.route("updateUserCoverImage").patch(verifyJWT, updateCoverImage);
+router.route("/getUserById/:id").get(verifyJWT, getUserById);
+router.route("/updateAccountDetails").patch(verifyJWT, updateAccountDetails);
+router.route("/updateUserAvatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/updateUserCoverImage").patch(verifyJWT,upload.single("coverImage"), updateUserCoverImage);
 
 
 
