510e81b0428fd1e3feb93e16158fe131ce0a4344
Shravan Yadav <yadavshravan9393@gmail.com>
2025-01-13 19:11:49 +0530

    feat: add user account management routes; remove unused frontend files

--- chat-backend/src/routes/user.routes.js
+++ chat-backend/src/routes/user.routes.js
@@ -36,6 +36,11 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/searchUser").get(verifyJWT, searchUser);
+router.route("/getUserById").get(verifyJWT, getUserById);
+router.route("updateAccountDetails").patch(verifyJWT, updateAccountDetails);
+router.route("updateUserAvatar").patch(verifyJWT, updateAvatar);
+router.route("updateUserCoverImage").patch(verifyJWT, updateCoverImage);
 
 
 
