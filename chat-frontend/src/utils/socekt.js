3afac9a019f5f87ff6a63d6f644b6967af26518a
Shravan Yadav <yadavshravan9393@gmail.com>
2025-01-17 20:46:47 +0530

    feat: add optional dependencies for bufferutil and utf-8-validate; remove unused socket.js file

--- chat-frontend/src/utils/socekt.js
+++
@@ -1,9 +0,0 @@
-// src/utils/socket.js
-import { io } from "socket.io-client";
-
-const socket = io("http://localhost:5000", {
-  path: "/socket.io/", // Ensure this matches your backend configuration
-  // transports: ["websocket"],
-});
-
-export default socket;
