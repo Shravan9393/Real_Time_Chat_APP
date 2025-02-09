5698be770d8fa94bffa3f7b34fa0ed3974f4a691
Shravan Yadav <yadavshravan9393@gmail.com>
2025-02-09 23:01:09 +0530

    feat: clean up ChatDashboard component by commenting out unused code and enhancing CSS styles

--- chat-frontend/src/components/Pages/chatDashboard.js
+++ chat-frontend/src/components/Pages/chatDashboard.js
@@ -9,26 +9,6 @@ const ChatDashboard = () => {
   const [fetchAgain, setFetchAgain] = useState(false);
   const { user } = ChatState();
 
-  //   return (
-  //     <div style={{ width: "100%", display: "flex", flexDirection: "column" }}>
-  //       {user && <SideNav />}
-  //       <div
-  //         style={{
-  //           display: "flex",
-  //           justifyContent: "space-between",
-  //           padding: "10px",
-  //           height: "91.5vh",
-  //         }}
-  //       >
-  //         {user && <MyChats fetchAgain={fetchAgain} />}
-  //         {user && (
-  //           <ChatWindow fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
-  //         )}
-  //       </div>
-  //     </div>
-  //   );
-  // };
-
   return (
     <div className="chat-dashboard">
       {user && <SideNav />}
